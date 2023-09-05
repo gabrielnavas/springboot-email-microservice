eb2fb5c803e7d34ab27d3f172089fdac844e0c6d
navas <gabrielnavas@email.com>
2023-09-05 18:09:56 -0300

    implements the simple mail message

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -5,6 +5,8 @@ import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.Email;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,7 +18,26 @@ public class EmailService {
     @Autowired
     private EmailRepository emailRepository;
 
+    @Autowired
+    private JavaMailSender emailSender;
+
     public Email sendEmail(Email emailModel) {
-        return emailRepository.save(emailModel);
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }
+        catch (Exception ex) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            emailModel = emailRepository.save(emailModel);
+        }
+        return emailModel;
     }
 }
